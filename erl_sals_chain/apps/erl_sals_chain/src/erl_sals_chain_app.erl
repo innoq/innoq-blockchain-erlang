9416104edd1476eb536ad9d80c91e66d36c5f58b
Simon Harrer <simon.harrer@gmail.com>
2018-04-19 13:11:13 +0200

    Hello world cowboy app

--- erl_sals_chain/apps/erl_sals_chain/src/erl_sals_chain_app.erl
+++ erl_sals_chain/apps/erl_sals_chain/src/erl_sals_chain_app.erl
@@ -15,7 +15,23 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    erl_sals_chain_sup:start_link().
+    {ok, Pid} = erl_sals_chain_sup:start_link(),
+    Routes = [ {
+        '_',
+        [
+            {"/", erl_sals_chain_root, []}
+        ]
+    } ],
+    Dispatch = cowboy_router:compile(Routes),
+
+    NumAcceptors = 10,
+    TransOpts = [ {ip, {0,0,0,0}}, {port, 2938} ],
+    ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+
+    {ok, _} = cowboy:start_http(chicken_poo_poo,
+        NumAcceptors, TransOpts, ProtoOpts),
+
+    {ok, Pid}.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
