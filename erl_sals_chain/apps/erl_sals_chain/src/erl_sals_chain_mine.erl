c982d0245035e2ef136120a998d2c263b4101839
Simon Harrer <simon.harrer@innoq.com>
2018-04-20 09:07:29 +0200

    Use millisecond for timestamps

--- erl_sals_chain/apps/erl_sals_chain/src/erl_sals_chain_mine.erl
+++ erl_sals_chain/apps/erl_sals_chain/src/erl_sals_chain_mine.erl
@@ -7,11 +7,11 @@ init(Req, _Opts) ->
   {cowboy_loop, Req, rumpelstielzchen}.
 
 info(_Msg, Req, State) ->
-  Timestamp = os:system_time(),
+  Timestamp = os:system_time(millisecond),
   Index = erl_sals_chain_keeper:get_index_of_last_block() + 1,
   PreviousBlockHash = erl_sals_chain_keeper:get_hash_of_last_block(),
   {Proof, NextBlock} = erl_sals_worker:mine(Index, Timestamp, "[]", PreviousBlockHash),
-  TimestampAfter = os:system_time(),
+  TimestampAfter = os:system_time(millisecond),
   Duration = (TimestampAfter - Timestamp) / 1000,
   HashingPower = Proof / Duration,
   erl_sals_chain_keeper:put_new_block(NextBlock),
