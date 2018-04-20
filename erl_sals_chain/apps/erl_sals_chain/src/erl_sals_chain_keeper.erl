57d3196097addc6f0a502ae239499d2bd553150e
Andreas Krüger <andreas.krueger@innoq.com>
2018-04-20 11:59:27 +0200

    Interface for replacement of entire chain.

--- erl_sals_chain/apps/erl_sals_chain/src/erl_sals_chain_keeper.erl
+++ erl_sals_chain/apps/erl_sals_chain/src/erl_sals_chain_keeper.erl
@@ -6,7 +6,7 @@
 
 % API:
 -export([get_index_of_last_block/0, get_hash_of_last_block/0, get_list_of_blocks/0,
-         put_new_block/1, confirmed_transaction/1, find_transaction/1]).
+         put_new_block/1, confirmed_transaction/1, find_transaction/1, replace_chain/1]).
 
 % gen_server:
 -export([init/1, handle_call/3, handle_cast/2, start_link/0]).
@@ -24,6 +24,9 @@ get_list_of_blocks() ->
 
 put_new_block(Block) ->
     gen_server:call(erl_sals_chain_keeper, {put_new_block, Block}).
+
+replace_chain(SortedListOfBlocks) ->
+    ok.
 
 confirmed_transaction(TransactionId) ->
     case find_transaction(TransactionId) of
